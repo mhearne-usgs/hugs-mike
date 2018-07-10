cb9ec107ca7c42b4725e7c0c2d23ba851f3d22b5
Michael Hearne <mhearne@usgs.gov>
2018-07-10 11:21:23 -0500

    Adding flake8 to travis.

--- hugs/calc/geology.py
+++ hugs/calc/geology.py
@@ -31,5 +31,6 @@ def snell_angle(incoming_angle, velocity_top, velocity_bottom):
     """
     if velocity_top == 0:
         raise ValueError('Top layer velocity can not be zero.')
-    sin_of_outgoing_ray = np.sin(np.radians(incoming_angle)) * (velocity_bottom / velocity_top)
+    sin_of_outgoing_ray = np.sin(np.radians(
+        incoming_angle)) * (velocity_bottom / velocity_top)
     return np.degrees(np.arcsin(sin_of_outgoing_ray))
